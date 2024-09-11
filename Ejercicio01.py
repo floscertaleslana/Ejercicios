aa6c566b32fa3678efa3a525b80382c4771961ce
floscertaleslana <floscertaleslana@gmail.com>
2024-09-11 18:13:40 +0200

    feat: Prueba eníio  ejercicios Python

---
+++ Ejercicio01.py
@@ -0,0 +1,14 @@
+"""
+1.- Crear una variable "MiEdad" y asignar el valor 25.
+2.- Crear una variable llamada "MiNombre" y asignar el valor "Juan".
+2.- Crear una variable llamada "MiPais" y asignar el valor "Argentina".
+2.- Crear una variable llamada "MiPresentacion" y asignar el valor de una cadena que incluya las variables "MiNombre" y "MiPais".
+"""
+MiEdad = 25
+print(MiEdad)
+MiNombre = 'Juan'
+print(MiNombre)
+MiPais = 'Argentina'
+print(MiPais)
+MiPresentacion = MiNombre + MiPais
+print(MiPresentacion)
